e41d6c6029e7b81836c375c9319d575b6c1e788b
jerry1938 <49976147+jerry1938@users.noreply.github.com>
2021-11-23 11:09:31 +0100

    Specified a timout for the notification.

--- .config/dunst/changeVolume.sh
+++ .config/dunst/changeVolume.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # changeVolum
 
-volume="$(pacmd list-sinks | awk '/\tvolume:/ {print $5}' | tail -n3 | head -n1 | cut -d '%' -f 1)"
-notify-send -a "changeVolume" -u low -h string:x-dunst-stack-tag:volume "Volume: ${volume}%"
+volume="$(pacmd list-sinks | awk '/\tvolume:/ {print $5}' | tail -n2 | head -n1 | cut -d '%' -f 1)"
+notify-send -a "changeVolume" -u low -t 2000 -h string:x-dunst-stack-tag:volume "Volume: ${volume}%"
